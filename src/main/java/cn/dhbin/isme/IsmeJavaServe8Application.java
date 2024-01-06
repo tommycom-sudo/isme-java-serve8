714b847b36443d2917c99026466bf3cf025429d4
thinkpad <249952@qq.com>
2024-01-06 17:04:06 +0800

    Initial commit

---
+++ src/main/java/cn/dhbin/isme/IsmeJavaServe8Application.java
@@ -0,0 +1,13 @@
+package cn.dhbin.isme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsmeJavaServe8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsmeJavaServe8Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/dhbin/isme/IsmeJavaServe8ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.dhbin.isme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsmeJavaServe8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
